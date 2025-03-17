46b940cb86065ec54364516ea8c395f61f831cd8
Misofu <misofu@gmail.com>
2025-03-17 18:08:37 -0300

    Update1 app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim();
+
+    if (nombreAmigo === ""){
+        alert("Por favor, inserte un Nombre");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+
+    actualizarLista();
+
+    inputAmigo.value = "";
+    inputAmigo.focus();
+
+
+}
